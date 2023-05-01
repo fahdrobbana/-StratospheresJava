48e23d5dfc68166ad018471c0560b723388eb10e
Jendoubi Majdi <majdi.jendoubi@esprit.tn>
2023-05-01 19:06:03 +0100

    second integration

--- src/users/gui/profil/ProfilController.java
+++ src/users/gui/profil/ProfilController.java
@@ -115,5 +115,18 @@ Image imgcer = new Image(u.getImage(),false) ;
              Logger.getLogger(ProfilController.class.getName()).log(Level.SEVERE, null, ex);
          }
     }
+
+    @FXML
+    private void Annonce(ActionEvent event) {
+                          try
+        {
+            Parent sv ;
+            sv = (AnchorPane)FXMLLoader.load(getClass().getResource("../Menu.fxml"));
+          pane.getChildren().removeAll() ; 
+          pane.getChildren().setAll(sv) ;                              
+        } catch (IOException ex) {
+             Logger.getLogger(ProfilController.class.getName()).log(Level.SEVERE, null, ex);
+         }
+    }
     
 }
